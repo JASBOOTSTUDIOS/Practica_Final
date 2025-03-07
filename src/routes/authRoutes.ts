9ae665f0195605b06f9353b9e17064e27719ed8b
Jose Luis Rojas Severino <jorojas@DCTIC.GC>
2025-03-07 08:56:36 -0400

    Nav del fron arreglado

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,6 +6,7 @@ import {
   updateCredentialsUserModel,
   Login,
   getCredentialsUserByIdModel,
+  getAllCredentialsUserModel,
 } from "../models/credentialUsersModel";
 import path from "path";
 
@@ -14,8 +15,9 @@ route.use(express.json());
 route.use(cors());
 route.use("/", express.static(path.join(__dirname, "../public")));
 
-// Ruta De Login.
+// Ruta De Credenciales.
 route.get("/profile", authMiddelware, getCredentialsUserByIdModel);
+route.get("/credentials", authMiddelware, getAllCredentialsUserModel);
 route.post("/login", Login);
 route.post("/register", authMiddelware, createCredentialsUserModel);
 route.put("/update", authMiddelware, updateCredentialsUserModel);
